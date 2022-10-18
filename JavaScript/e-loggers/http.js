46bbc3cf2b9ef7ad812c109662ffcf40e89e4b6d
Denis Kovalyov <denys.kovalov@auto1.com>
2022-10-18 20:51:18 +0300

    Integrate pino logger (#5)

---
+++ JavaScript/e-loggers/http.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const http = require('node:http');
+
+const NOT_FOUND = 'Not found';
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*',
+  'Content-Type': 'text/plain',
+};
+
+const setHeaders = (res) => {
+  for (const header of Object.keys(headers)) res.setHeader(header, headers[header]);
+  return res;
+};
+
+const receiveArgs = async (req) => {
+  const buffers = [];
+  for await (const chunk of req) buffers.push(chunk);
+  const data = Buffer.concat(buffers).toString();
+  return JSON.parse(data);
+};
+
+module.exports = (routing, port) => {
+  http.createServer(async (req, res) => {
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204, headers).end();
+      return;
+    }
+
+    setHeaders(res);
+    const { url, socket } = req;
+    const [name, method, id] = url.substring(1).split('/');
+    const entity = routing[name];
+    if (!entity) return res.end(NOT_FOUND);
+    const handler = entity[method];
+    if (!handler) return res.end(NOT_FOUND);
+    const src = handler.toString();
+    const signature = src.substring(0, src.indexOf(')'));
+    const args = [];
+    if (signature.includes('(id')) args.push(id);
+    if (signature.includes('{')) args.push(await receiveArgs(req));
+    console.log(`${socket.remoteAddress} ${method} ${url} ${JSON.stringify(args)}`);
+    const result = await handler(...args);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result.rows));
+  }).listen(port);
+
+  console.log(`API on port ${port}`);
+};
---
+++ JavaScript/e-loggers/pino.js
@@ -0,0 +1,49 @@
+const { LOGS_PATH, LOG_LEVEL } = require('./config');
+const path = require('node:path');
+const fs = require('node:fs');
+const util = require('node:util');
+const pino = require('pino');
+const pretty = require('pino-pretty');
+
+const levels = {
+  debug: 10,
+  log: 20,
+  dir: 30,
+  error: 50,
+  system: 60,
+  access: 70,
+};
+
+const date = new Date().toISOString().substring(0, 10);
+const filePath = path.join(LOGS_PATH, `${date}.log`);
+
+if (!fs.existsSync(LOGS_PATH)){
+  fs.mkdirSync(LOGS_PATH);
+};
+
+const streams = [
+  { stream: pretty(), level: 'debug' },
+  { stream: fs.createWriteStream(filePath, { flags: 'a' }), level: 'debug' },
+];
+
+const logger = pino({
+    level: LOG_LEVEL || 'debug',
+    customLevels: levels,
+    useOnlyCustomLevels: true,
+    formatters: {
+      level: (label) => {
+        return { level: label === 'log' ? 'info' : label };
+      },
+
+    },
+    hooks: {
+      logMethod (args, method) {
+        const message = util.format(...args);
+        return method.call(this, message);
+      },
+    },
+  },
+  pino.multistream(streams, { levels }),
+);
+
+module.exports = logger;
